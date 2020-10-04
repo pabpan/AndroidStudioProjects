c3585f59674a160af26210937f7745ba6f44011e
pabpan <pabpan@alu.iesserpis.org>
2020-10-04 12:12:51 +0200

    Ejercicio 1 - Hola Mundo

---
+++ AndroidStudioProjects/Hola_mundo_1/app/src/main/java/pablo/suarez/hola_mundo_1/MainActivity.java
@@ -0,0 +1,14 @@
+package pablo.suarez.hola_mundo_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
