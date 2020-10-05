770ca8b68b8964912a08c47c678133f366cdee54
pabpan <pabpan@alu.iesserpis.org>
2020-10-05 11:27:59 +0200

    Actualizado 05/10/20 - 11:27

---
+++ AndroidStudioProjects/Saludar_Final/app/src/main/java/pablo/suarez/saludar_final/Pantalla_2.java
@@ -0,0 +1,14 @@
+package pablo.suarez.saludar_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_2);
+    }
+}
